9c02749acede77ef9be9b3a866e23569eef809d7
Mark Koester <mwkteacher@gmail.com>
2011-10-01 23:14:09 +0800

    initial commit

---
+++ templates/embedded_piratepad.tpl.php
@@ -0,0 +1,4 @@
+<div style="background:#fff;padding:3px">
+<iframe src='<?php print $url; ?>' width=<?php print $width; ?> height=<?php print $height; ?>>
+<div class="piratepad_link"><a href="<?php print $url_id; ?>"><b>Check Out this PiratePad at: </b></a></div>
+</div>
